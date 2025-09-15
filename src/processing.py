f99ba9ce3f59c7774bebf6daffea6d487afe86cf
Максим Пак <dodecaedr2000@gmail.com>
2025-09-15 22:49:21 +0400

    correct processing.py

--- src/processing.py
+++ src/processing.py
@@ -10,29 +10,31 @@ def filter_by_state(list_dictionary: list, state='EXECUTED') -> list:
     return list_clean
 
 
-def sort_by_date(list_dictionary: list, ascending=False) -> list:
+def sort_by_date(list_dictionary: list, reverse=True) -> list:
     """ Сортирует список словарей по дате """
-    return sorted(list_dictionary, key=lambda d: datetime.fromisoformat(d["date"]), reverse=not ascending)
+    return sorted(list_dictionary, key=lambda d: datetime.fromisoformat(d.get('date')), reverse=reverse)
 
 
-# Данные с одинарными кавычками
-input_data = input()
+if __name__ == '__main__':
 
-# Преобразовываем одинарные кавычки в двойные
-fixed_data = input_data.replace("'", '"')
+    test = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ]
 
-# Преобразуем строку обратно в список словарей
-user_dictionary = eval(fixed_data)
+    # Данные с одинарными кавычками
+    input_data = test
 
-# Применяем фильтры и сортировку
-filtered_executed = filter_by_state(user_dictionary)
-sorted_descending = sort_by_date(filtered_executed)
+    # Применяем фильтры и сортировку
+    filtered_executed = filter_by_state(input_data)
+    sorted_descending = sort_by_date(filtered_executed)
 
-# Вывод результатов
-print("Отфильтрованные операции (только EXECUTED):")
-print(filtered_executed)
+    # Вывод результатов
+    print("Отфильтрованные операции (только EXECUTED):")
+    print(filtered_executed)
 
-print("\nОтсортированные операции (по убыванию):")
-print(sorted_descending)
+    print("\nОтсортированные операции (по убыванию):")
+    print(sorted_descending)
 
-# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
