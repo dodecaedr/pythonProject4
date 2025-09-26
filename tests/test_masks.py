2a0cc78e2f1817db41199e90c78094ad442a0e57
Максим Пак <dodecaedr2000@gmail.com>
2025-09-26 22:10:59 +0400

    correct tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,21 +1,28 @@
-from pytest import mark
+import pytest
 
 from src.masks import get_mask_account, get_mask_card_number
 
 
-@mark.parametrize('card, mask', [
-       ('7000792289606361', '7000 79** **** 6361'),
-       ('7600924456123', '7600 92** **** 6123'),
-       ('430000000000077722', '4300 00** **** 7722'),
-       ('', 'Некорректный номер карты'),
-])
-def test_get_mask_card_number(card, mask):
+@pytest.mark.parametrize(
+    "card, mask",
+    [
+        ("7000792289606361", "7000 79** **** 6361"),
+        ("7600924456123", "7600 92** **** 6123"),
+        ("430000000000077722", "4300 00** **** 7722"),
+        ("123", "Некорректный номер карты"),
+    ],
+)
+def test_get_mask_card_number(card: str, mask: str) -> None:
     assert get_mask_card_number(card) == mask
 
 
-def test_get_mask_account(fix_get_mask_account):
-    assert get_mask_account(fix_get_mask_account) == '**4305'
+def test_get_mask_card_number_empty(fix_get_mask_card_number_empty: str) -> None:
+    assert get_mask_account(fix_get_mask_card_number_empty) == "Вы ничего не ввели"
 
 
-def test_get_mask_account_empty(fix_get_mask_account_empty):
-    assert get_mask_account(fix_get_mask_account_empty) == 'Вы ничего не ввели'
+def test_get_mask_account_empty(fix_get_mask_account_empty: str) -> None:
+    assert get_mask_account(fix_get_mask_account_empty) == "Вы ничего не ввели"
+
+
+def test_get_mask_account(fix_get_mask_account: str) -> str:  # masks
+    assert get_mask_account(fix_get_mask_account) == "**4305"
