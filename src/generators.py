991808eca5781dfb9f1bd9a1dfaef28f082d2706
Максим Пак <dodecaedr2000@gmail.com>
2025-11-14 03:03:06 +0400

    add transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -4,3 +4,9 @@ from typing import Any, Iterator
 def filter_by_currency(transactions: list[dict[str, Any]], currency: str) -> Iterator[dict[str, Any]]:
     """Фильтрация транзакций по валюте"""
     return filter(lambda x: x.get('operationAmount', {}).get('currency', {}).get('code', '') == currency, transactions)
+
+
+def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[str]:
+    """Описание операций"""
+    for transaction in transactions:
+        yield transaction.get('description')
