98fc874d5e63e58c04aad575dc8ac01869048e28
Максим Пак <dodecaedr2000@gmail.com>
2026-03-05 16:37:26 +0400

    add decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,39 @@
+import functools
+import sys
+
+
+def log(filename=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            func_name = func.__name__
+
+            # Выбираем место для записи логов
+            if filename:
+                output = open(filename, 'a')
+            else:
+                output = sys.stdout
+
+            try:
+                result = func(*args, **kwargs)
+                # Пишем успех в лог
+                output.write(f"{func_name} ok\n")
+                return result
+
+            except Exception as e:
+                # Пишем тип и входные данные ошибки
+                error_type = type(e).__name__
+                output.write(
+                    f"{func_name} error: {error_type}. "
+                    f"Inputs: {args}, {kwargs}\n"
+                )
+                raise  # Передаём ошибку дальше
+
+            finally:  # Закрываем файл, если писали в него
+
+                if filename:
+                    output.close()
+
+        return wrapper
+
+    return decorator
